f7c8e2fcd67a2aa40963bf033bd8d802c5c2c555
renaud8888 <renaud.pgm8@gmail.com>
2026-04-21 00:31:39 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
-  typedRoutes: true,
-  outputFileTracingRoot: path.join(__dirname, "../..")
+  typedRoutes: true
 };
 
 export default nextConfig;
