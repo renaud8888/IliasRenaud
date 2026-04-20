b0e41b34406de45b5dbe61f6d0cc177d7dbf655f
renaud8888 <renaud.pgm8@gmail.com>
2026-04-21 00:23:13 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,6 +4,7 @@ import { AdminPanel } from "@/components/forms/admin-panel";
 import { isAuthenticated } from "@/lib/auth";
 import { getAdminData } from "@/lib/services/dashboard";
 
+
 export default async function AdminPage() {
   const authenticated = await isAuthenticated();
 
